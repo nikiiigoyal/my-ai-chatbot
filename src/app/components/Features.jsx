af1062975832415d0e3c7f8f6c49ff8d00400960
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-05 22:56:32 +0530

    Add image

---
+++ src/app/components/Features.jsx
@@ -0,0 +1,9 @@
+export default function Features() {
+  return (
+    <>
+      <div className="w-full mx-auto px-14 py-9 bg-black">
+        <img src="./Hero.png"></img>
+      </div>
+    </>
+  );
+}
