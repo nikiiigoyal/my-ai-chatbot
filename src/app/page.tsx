d4e714f5b78056fcc2370bfc72e8259ce29af5eb
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-05 22:31:15 +0530

    Add to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Header from './components/Header'
+import HeroLeft from './components/HeroLeft'
+import ChatDemo from './components/HeroRight'
 
 export default function Home() {
   return (
     <>
     <Header />
+    <div className='my-10 mx-auto grid grid-cols-1 lg:grid-cols-2 w-[80%] border border-gray-800'>
+    <HeroLeft />
+    <ChatDemo />
+    </div>
     </>
   );
 }
