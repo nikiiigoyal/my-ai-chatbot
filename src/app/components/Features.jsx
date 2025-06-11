15655d4cb64c4c4443e8ac28ea359cdd73eb6724
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-11 09:20:52 +0530

    Made fetures section responsive

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -1,8 +1,12 @@
 export default function Features() {
   return (
     <>
-      <div className="w-full mx-auto border-b border-gray-800 px-14 py-9 bg-black">
-        <img src="./Hero.png"></img>
+      <div className="w-full mx-auto border-b border-gray-800 px-4 sm:px-6 md:px-8 lg:px-14 py-6 sm:py-8 md:py-9 bg-black">
+        <img
+          src="./Hero.png"
+          alt="Hero"
+          className="w-full h-auto max-w-none object-contain"
+        />
       </div>
     </>
   );
