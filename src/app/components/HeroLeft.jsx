a0088d3d369970fe6c14dfcd0719d59fa6ddd2d0
nikiiigoyal <nikiiigoyal@gmail.com>
2025-07-21 12:18:37 +0530

    modified heroleft

--- src/app/components/HeroLeft.jsx
+++ src/app/components/HeroLeft.jsx
@@ -2,23 +2,39 @@ import { ChevronRight } from "lucide-react";
 
 const HeroLeft = () => {
   return (
-    <div className="flex flex-col justify-center h-full px-8 lg:px-16 mb-8 bg-black">
-      <h1 className="text-6xl lg:text-7xl font-bold text-white mb-8 leading-tight sm:text-center">
+    <div className="flex flex-col justify-center h-full px-8 lg:px-16 mb-8 bg-black font-sans">
+      <h1 className="text-6xl font-bold text-white mb-[.625rem] leading-tight">
         Chat SDK
       </h1>
 
-      <p className="text-xl lg:text-2xl text-gray-400 mb-12 leading-relaxed max-w-lg">
+      <p className="text-xl font-medium md:mb-12 text-[hsla(0,0%,63%,1)] mb-4 leading-[32px] max-w-lg ">
         Chat SDK is a free, open-source template that helps you dive right into
         building powerful chatbot applications.
       </p>
 
-      <div className="flex flex-col sm:flex-row gap-4 pb-5">
-        <button className="flex items-center justify-center gap-3 bg-white text-black px-8 py-4 rounded-xl font-semibold text-lg hover:bg-gray-100 transition-colors duration-200">
+      <div className="flex flex-col sm:flex-row gap-4 pb-3">
+        <button
+          className="flex items-center max-w-100 bg-[hsla(0,0%,93%,1)] px-[18px] sm:px-[10px] sm:h-[40px] py-2 rounded-md border border-gray-700  transition-[border-color,background,color,transform,box-shadow] duration-150 ease-in-out font-medium text-[hsla(0,0%,4%,1)]  hover:bg-[#ccc]    
+  text-xs
+  leading-[1.25rem]
+          h-[48px]     
+  cursor-pointer 
+  shadow-xs
+  hover:opacity-90"
+        >
           Get Started
-          <ChevronRight size={20} />
+          <ChevronRight size={16} />
         </button>
 
-        <button className="flex items-center justify-center gap-3 bg-transparent border border-gray-600 text-white px-8 py-4 rounded-xl font-semibold text-lg hover:bg-gray-800 transition-colors duration-200">
+        <button
+          className="flex items-center max-w-100 bg-[hsla(0,0%,4%,1)] lg:px-4 sm:px-[10px] sm:h-[40px] py-5 rounded-md border border-[hsla(0,0%,100%,0.14)]  transition-[border-color,background,color,transform,box-shadow] duration-100 ease-in-out font-medium text-[hsla(0,0%,93%,1)]  hover:bg-[hsla(0,0%,100%,.09)]    
+  text-sm
+  leading-[1.25rem]
+          h-[48px]     
+  cursor-pointer 
+  shadow-xs
+  hover:opacity-90"
+        >
           Try It Out
         </button>
       </div>
